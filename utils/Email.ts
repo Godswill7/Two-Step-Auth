4693ad3efd0ac06b2242060b50b7bb7bf1fc37dc
Godswill Udida <udidagodswill7@gmail.com>
2024-04-08 14:38:26 +0100

    dotenv variables

--- utils/Email.ts
+++ utils/Email.ts
@@ -1,19 +1,18 @@
 import { google } from "googleapis";
-import { oauth2 } from "googleapis/build/src/apis/oauth2";
 import nodemailer from "nodemailer";
 import path from "path";
 import ejs from "ejs";
 import jwt from "jsonwebtoken";
+import env from "dotenv";
+env.config();
 
-const GOOGLE_ID =
-  "199704572461-84filrl6gfs1cie7b5bkvspne91bbj0q.apps.googleusercontent.com";
+const GOOGLE_ID = process.env.G_ID!;
 
-const GOOGLE_SECRET = "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";
+const GOOGLE_SECRET = process.env.G_SECRET!;
 
-const GOOGLE_URL = "https://developers.google.com/oauthplayground/";
+const GOOGLE_URL = process.env.G_URL!;
 
-const GOOGLE_TOKEN =
-  "1//04VN_6MSoOhbqCgYIARAAGAQSNwF-L9IreLEe5iU5Hd_DoUnio4gsaH3CBXozNlnoofjcBGyAVetbZSh0e2DR2rAr6TGXKCyRkgs";
+const GOOGLE_TOKEN = process.env.G_TOKEN!;
 
 const oAuth = new google.auth.OAuth2(GOOGLE_ID, GOOGLE_SECRET, GOOGLE_URL);
 oAuth.setCredentials({ access_token: GOOGLE_TOKEN });
@@ -26,7 +25,7 @@ export const sendMail = async (user: any) => {
       service: "gmail",
       auth: {
         type: "OAuth2",
-        user: "peterotunuya2@gmail.com",
+        user: "udidagodswill7@gmail.com",
         clientId: GOOGLE_ID,
         clientSecret: GOOGLE_SECRET,
         refreshToken: GOOGLE_TOKEN,
@@ -34,18 +33,16 @@ export const sendMail = async (user: any) => {
       },
     });
 
-    // const url = `http://localhost:5173/api/${user.id}/verify`;
-
     const token = jwt.sign(
       {
         id: user.id,
       },
-      "code"
+      process.env.SECRET!
     );
 
     const url = `http://localhost:1122/api`;
     const choiceData = {
-    name : user.name,
+      name: user.name,
       url: `${url}/${token}/verify`,
     };
 
@@ -53,9 +50,9 @@ export const sendMail = async (user: any) => {
     const realData = await ejs.renderFile(data, choiceData);
 
     const mailer = {
-      from: "peterotunuya2@gmail.com",
+      from: "udidagodswill7@gmail.com",
       to: user.email,
-      subject: "Congrate",
+      subject: "Welcome to Our Platform",
       html: realData,
     };
 
